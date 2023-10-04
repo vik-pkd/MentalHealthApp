27521f1f41017ecce7553d36b8328edad00464a3
Vikas <112001049@smail.iitpkd.ac.in>
2023-10-04 20:46:33 +0530

    backend initial script and gitignore added

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
