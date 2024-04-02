c839bf8e7fae9bb0b2410f81c3602567686f4f2c
Vikas <112001049@smail.iitpkd.ac.in>
2024-04-03 03:02:22 +0530

    medicine taken added

--- frontend/src/constants/styles.ts
+++ frontend/src/constants/styles.ts
@@ -3,6 +3,12 @@ import { StyleSheet } from "react-native";
 const globalStyles = StyleSheet.create({
     blackText: {
         color: 'black'
+    },
+    themeBackGroundColor: {
+        color: 'rgba(134, 65, 244, 1)'
+    },
+    themeColor: {
+        color: 'rgba(134, 65, 244, 1)'
     }
 });
 
